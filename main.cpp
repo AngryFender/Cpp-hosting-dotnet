76f2202b2e91071ce141e086cac934c22c00d4db
AngryFender <ece00356@myport.ac.uk>
2023-01-02 02:38:10 +0000

    Added loading of Navite AOT DotNet binary in managed hosting app. Added consuming of static DotNet function in managed hosting app

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <dlfcn.h>
+#include <cassert>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    //open the native AOT DotNet binary from LD_LIBRARY_PATH
+    void *dotnet_handle;
+    dotnet_handle = dlopen("Dotnet_library.so",RTLD_LAZY| RTLD_DEEPBIND);
+
+    if(!dotnet_handle)
+    {
+        std::cout<<"Error: "<<dlerror();
+    }
+    else
+    {
+        //Load static function with identical symbol name
+        typedef int(*ptr_absolute)(int);
+        ptr_absolute absolute =  (ptr_absolute)dlsym(dotnet_handle, "absolute");
+
+        std::cout<<"DotNet Math.Abs(-100) = "<<absolute(-100)<<std::endl;
+        std::cout<<"Std::abs(-100) = "<<std::abs(-100)<<std::endl;
+        dlclose(dotnet_handle);
+    }
     return 0;
 }
